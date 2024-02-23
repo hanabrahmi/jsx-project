b26e3dc140266744ada4ce43a92de1c54d1d779d
hana brahmi <brahmihana7@gmail.com>
2024-02-23 17:09:30 +0100

    jsx

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ function App() {
      <div style={{ border: 'solid 1px black', maxWidth: '100vw' }}>
       <h1 className="title red">Your name here</h1>
       <br />
-      <h1>image in src</h1>
+      <h4>image in src</h4>
       <img src={imageInsrc} alt="Image in src" className="image" />
       <br />
-      <h1>image in public</h1>
+      <h4>image in public</h4>
       <img src="/imageInpublic.png" alt="Image in public" className="image" />
       <br />
       
